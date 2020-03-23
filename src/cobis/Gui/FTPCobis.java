728c41865c624cd75c43a79a07bf2155f6d66f99
yisheng <yileone@gmail.com>
2020-03-23 14:11:12 -0400

    prueba jar

--- src/cobis/Gui/FTPCobis.java
+++ src/cobis/Gui/FTPCobis.java
@@ -1,27 +1,27 @@
 /**
+
  * 
  */
 package cobis.Gui;
 
 /**
- * @author arte
+ * @author a
  *
  */
 public class FTPCobis {
 
-	/**
-	 * 
-	 */
-	public FTPCobis() {
-		// TODO Auto-generated constructor stub
-	}
+private static String error="la opción no es válida ejecute el parametro -H para mostrar la ayuda"; 
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if (args.length>1)
+		{
+			System.out.println(error);
+		}
+		
 	}
 
 }
