f164cd6e273ac5f9b60abe87ecc1979c9599c525
yisheng <yileone@gmail.com>
2020-03-23 14:23:18 -0400

    menu ayuda

--- src/cobis/Gui/FTPCobis.java
+++ src/cobis/Gui/FTPCobis.java
@@ -10,7 +10,7 @@ package cobis.Gui;
  */
 public class FTPCobis {
 
-private static String error="la opción no es válida ejecute el parametro -H para mostrar la ayuda"; 
+private static String error="la opción no es válida ejecute el parametro -? para mostrar la ayuda"; 
 
 	/**
 	 * @param args
@@ -21,6 +21,38 @@ private static String error="la opción no es válida ejecute el parametro -H pa
 		{
 			System.out.println(error);
 		}
+		else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else if (args[0].equals("-?")) {
+			mostrarAyuda();
+		}else  {
+			System.out.println(error);
+		}
+		
+	}
+/**
+ * Muestra todos los menús con sus opciones de ejecucion
+ */
+	private static void mostrarAyuda() {
+		// TODO Auto-generated method stub
+		System.out.println("********************************");
+		System.out.println("***FtP Cobis - Naiguata V1.0****");
+		System.out.println("********************************");
+		System.out.println("* -? la ayuda que esta leyendo *");
+		System.out.println("* -P la ayuda que esta leyendo *");
+		System.out.println("*  ? la ayuda que esta leyendo *");
+		System.out.println("*  ? la ayuda que esta leyendo *");
+		System.out.println("*  ? la ayuda que esta leyendo *");
+		System.out.println("*  ? la ayuda que esta leyendo *");
+		System.out.println("********************************");
 		
 	}
 
