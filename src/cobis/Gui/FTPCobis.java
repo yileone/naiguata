88cca51adb7827e45160f97f7f3bcd10baef500c
yisheng <yileone@gmail.com>
2020-03-24 14:47:45 -0400

    SUBIENndo archivo ftp

--- src/cobis/Gui/FTPCobis.java
+++ src/cobis/Gui/FTPCobis.java
@@ -17,6 +17,16 @@ public class FTPCobis {
 	private static String error = "la opción no es válida ejecute el parametro -? para mostrar la ayuda";
 	private static Propiedades propiedades;
 
+	private static void cargarArchivo() {
+		cargarPropiedades();
+		final Archivo archivo = new Archivo(propiedades, 'S');
+		final FtpNaiguata conexion = new FtpNaiguata(archivo);
+		if (conexion.isLogin()) {
+			conexion.putFile();
+			conexion.cerrar();
+		}
+	}
+
 	private static void cargarPropiedades() {
 		// TODO Auto-generated method stub
 		propiedades = new Propiedades();
@@ -36,8 +46,8 @@ public class FTPCobis {
 			obtenerArchivo();
 		} else if (args[0].equals("-T")) {
 			probarConexion();
-		} else if (args[0].equals("-?")) {
-			mostrarAyuda();
+		} else if (args[0].equals("-C")) {
+			cargarArchivo();
 		} else if (args[0].equals("-?")) {
 			mostrarAyuda();
 		} else if (args[0].equals("-?")) {
@@ -60,7 +70,7 @@ public class FTPCobis {
 		System.out.println("* -P muestras las propiedades del archivo de configuraión *");
 		System.out.println("* -O obtiene el archivo de entrada del servidor FTP *");
 		System.out.println("* -T prueba la conexion con el servidor FTP *");
-		System.out.println("*  ? la ayuda que esta leyendo *");
+		System.out.println("* -C carga el archivo de salida al servidor FTP *");
 		System.out.println("*  ? la ayuda que esta leyendo *");
 		System.out.println("********************************");
 
--- src/conexion/FtpNaiguata.java
+++ src/conexion/FtpNaiguata.java
@@ -5,10 +5,12 @@ package conexion;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Calendar;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -55,6 +57,14 @@ public class FtpNaiguata {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public String fecha() {
+		String fecha = "";
+		Calendar.getInstance();
+		fecha = fecha + Calendar.YEAR + Calendar.MONTH + Calendar.DATE + Calendar.HOUR + Calendar.MINUTE
+				+ Calendar.SECOND;
+		return fecha;
 	}
 
 	/**
@@ -107,7 +117,7 @@ public class FtpNaiguata {
 			outputStream.close();
 
 			if (success) {
-				System.out.println("File #1 has been downloaded successfully.");
+				System.out.println("File " + nombreArchivo + " ha sido descargado exitosamente.");
 			}
 
 		} catch (final FileNotFoundException e) {
@@ -125,6 +135,40 @@ public class FtpNaiguata {
 	 */
 	public boolean isLogin() {
 		return login;
+	}
+
+	/**
+	 * envia el archivo al servidor FTP y que se encuetnra la carpeta de Salida
+	 *
+	 */
+	public void putFile() {
+		// final String remote_working_dir_path = "/ruta/remota/del/ftp/";
+		final String directorio = archivo.getPropiedades().getCarpSalida();
+		final String nombreArchivo = archivo.getPropiedades().getArchivoSalida();
+		File uploadFile;
+		if (directorio.isEmpty() || directorio.equals("") || directorio == null) {
+			uploadFile = new File(nombreArchivo);
+		} else {
+			uploadFile = new File(directorio + "/" + nombreArchivo);
+		}
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(uploadFile);
+			cliente.enterLocalPassiveMode(); // IMPORTANTE!!!!
+			cliente.setFileType(FTP.BINARY_FILE_TYPE);
+			// cliente.changeWorkingDirectory(remote_working_dir_path);
+			final boolean sucess = cliente.storeFile(nombreArchivo, fis);
+			fis.close();
+			if (sucess == false) {
+				throw new Exception("Error al subir el fichero");
+			} else {
+				System.out.println("File " + uploadFile + " ha sido cargado exitosamente.");
+
+			}
+		} catch (final Exception eFTPClient) {
+			System.out.println(eFTPClient.getMessage());
+		}
+
 	}
 
 	/**
